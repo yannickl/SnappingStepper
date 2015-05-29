0f713df50e0ec4f604eaab4afb7689274d349d29
Yannick Loriot <yannick.loriot@gmail.com>
2015-05-29 18:06:26 +0200

    [UPDATE] Test cases

--- Tests/SnappingStepperTests.swift
+++ Tests/SnappingStepperTests.swift
@@ -13,14 +13,19 @@ class SnappingStepperTests: XCTestCase {
   func testDefaultValues() {
     let stepper = SnappingStepper()
 
-    XCTAssert(stepper.autorepeat, "'Autorepeat' attributes should be false by default")
-    XCTAssert(!stepper.wraps, "'Wraps' attributes should be false by default")
+    XCTAssert(stepper.continuous, "'continuous' attributes should be false by default")
+    XCTAssert(stepper.autorepeat, "'autorepeat' attributes should be false by default")
+    XCTAssert(!stepper.wraps, "'wraps' attributes should be false by default")
+    XCTAssert(stepper.minimumValue == 0, "'minimumValue' attributes should be equal to 0 by default")
+    XCTAssert(stepper.maximumValue == 100, "'maximumValue' attributes should be equal to 100 by default")
+    XCTAssert(stepper.stepValue == 1, "'stepValue' attributes should be equal to 1 by default")
+    XCTAssert(stepper.value == 0, "'value' attributes should be equal to 0 by default")
   }
 
   func testMaximumValue() {
     let stepper = SnappingStepper()
 
 
-    XCTAssert(stepper.maximumValue >= stepper.minimumValue, "'Maximum' value should always be AlwaysGreaterOrEqual than the 'minimum' value")
+    XCTAssert(stepper.maximumValue >= stepper.minimumValue, "'maximum' value should always be AlwaysGreaterOrEqual than the 'minimum' value")
   }
 }
